7a7c098d820612661f977971c5991baa6c034ed5
Francisco Sancho <frsancho1985@gmail.com>
2023-01-04 21:57:28 -0300

    Adicionando questões level A1

--- src/assets/data.js
+++ src/assets/data.js
@@ -1,6 +1,6 @@
 const data = [
   {
-    question: "Wich sentence is correct?",
+    question: "Which sentence is correct?",
     incorrectAnswers: [
       "He live on London",
       "He live in London",
@@ -9,13 +9,76 @@ const data = [
     correctAnswer: "He lives in London",
   },
   {
-    question: "Wich sentence is correct?",
+    question: "Which sentence is correct?",
     incorrectAnswers: [
       "I doesn't like coffee",
       "I does like coffee",
     ],
     correctAnswer: "I don't like coffee",
   },
-  
+  {
+    question: "Which sentence is correct?",
+    incorrectAnswers: [
+      "He goes to work in bus",
+      "He goes to work by the bus",
+    ],
+    correctAnswer: "He goes to work by bus",
+  },
+  {
+    question: "Which sentence is correct",
+    incorrectAnswers: [
+      "George has very big car",
+      "George a very big car has"
+    ],
+    correctAnswer: "George has a very big car"
+  },
+  {
+    question: "Which sentence is correct?",
+    incorrectAnswers: [
+      "Look at this house over there",
+      "Look at that house over here",
+    ],
+    correctAnswer: "Look at that house over there",
+  },
+  {
+    question: "Which sentence is correct?",
+    incorrectAnswers: [
+      "How he does go to school?",
+      "Does he go to school how?",
+    ],
+    correctAnswer: "How does he go to school?"
+  },
+  {
+    question: "Which sentence is correct?",
+    incorrectAnswers: [
+      "Are you going party on Friday?",
+      "Are you going partying on Friday?",
+    ],
+    correctAnswer: "Are you going to party on Friday?",
+  },
+  {
+    question: "Which sentence is correct?",
+    incorrectAnswers: [
+      "I really should to talk to her",
+      "I really must to talk to her",
+    ],
+    correctAnswer: "I really need to talk to her",
+  },
+  {
+    question: "Which sentence is correct?",
+    incorrectAnswers: [
+      "Did you rather go to a different movie?",
+      "Would you rather to go to a different movie?",
+    ],
+    correctAnswer: "Would you rather go to a different movie?",
+  },
+  {
+    question: "Which sentence is correct? I was born ...",
+    incorrectAnswers: [
+      "before 30 years",
+      "the last 30 years",
+    ],
+    correctAnswer: "30 years ago",
+  }
 
 ]
