439da479ff6ca2978d156199b4f607e9f287d745
Francisco Sancho <frsancho1985@gmail.com>
2023-01-04 21:41:50 -0300

    Criando o projeto

---
+++ README.md
@@ -0,0 +1,2 @@
+Projeto desenvolvido com a intenção de realizar um teste de nível de inglês.
+* Não é válido como certificado de proeficiência em inglês.
---
+++ src/assets/data.js
@@ -0,0 +1,21 @@
+const data = [
+  {
+    question: "Wich sentence is correct?",
+    incorrectAnswers: [
+      "He live on London",
+      "He live in London",
+      
+    ],
+    correctAnswer: "He lives in London",
+  },
+  {
+    question: "Wich sentence is correct?",
+    incorrectAnswers: [
+      "I doesn't like coffee",
+      "I does like coffee",
+    ],
+    correctAnswer: "I don't like coffee",
+  },
+  
+
+]
